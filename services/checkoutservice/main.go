a3696d0d8653a8383c6024d16b8549f8b882716e
Alexander den Oudsten <alex@alexonline.nl>
2019-08-15 15:06:35 +0200

    created paymentservice

--- services/checkoutservice/main.go
+++ services/checkoutservice/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Checkout represents the information required to perform a succesfull checkout.
+type Checkout struct {
+	SessionID  string `json:"sessionid" binding:"required"`
+	Address    string `json:"address" binding:"required"`
+	Email      string `json:"email" binding:"required"`
+	Creditcard string `json:"creditcard" binding:"required"`
+}
+
+func checkout(c *gin.Context) {
+
+	// Get the JSON data
+	var checkout Checkout
+	if err := c.ShouldBindJSON(&checkout); err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	getShoppingCart()
+	payProducts()
+	shipProducts()
+	sendEmail()
+
+	return
+}
+
+// setupRouter initializes our HTTP routes
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+	router.POST("/checkout", checkout)
+	return router
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	// Start HTTP server
+	gin.SetMode(gin.ReleaseMode)
+	r := setupRouter()
+	log.Println("Server started. Now accepting connections...")
+	r.Run(":8080")
 }
--- services/paymentservice/app.py
+++ services/paymentservice/app.py
@@ -1,11 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import time
+import random
 app = Flask(__name__)
 
+class Payment:
 
-@app.route('/')
+	def __init__(self, creditcard, paymentid):
+		self.creditcard = creditcard
+		self.paymentid = id
+
+payments = []
+
+@app.route('/payment', methods=['POST'])
 def index():
-    return render_template('index.html')
+	# Get the JSON data
+	data = request.get_json()
+
+	# Simulate a payment processing
+	time.sleep(1)
+
+	# Save the payment
+	np = Payment(creditcard=data["creditcard"], paymentid=random.randint(1, 100000))
+	payments.append(np)
+
+	# Make JSON response
+	payload = {
+		"status": "success"
+	}
+	payload = jsonify(payload)
+
+	# Return payment
+	return payload, 201
+
 
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8000, debug=True)
+  app.run(host='0.0.0.0', port=8000, debug=True)
  
