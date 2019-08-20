b19574f25d9b91fc65f602641c7caba2e1118fee
Alexander den Oudsten <alex@alexonline.nl>
2019-08-20 15:48:35 +0200

    fix products test

--- services/productservice/main_test.go
+++ services/productservice/main_test.go
@@ -3,15 +3,30 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"math/rand"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestCreateProduct(t *testing.T) {
+	router := setupRouter()
+	w := httptest.NewRecorder()
+
+	p := Product{
+		SKU:         "SKU1",
+		Name:        "test1",
+		Price:       22,
+		Description: "used for testing",
+	}
+	pjson, _ := json.Marshal(p)
+
+	req, _ := http.NewRequest("POST", "/product", bytes.NewBuffer(pjson))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 201, w.Code)
+}
 
 func TestGetAllProducts(t *testing.T) {
 	router := setupRouter()
@@ -31,29 +46,4 @@ func TestGetProduct(t *testing.T) {
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
-}
-
-func TestCreateProduct(t *testing.T) {
-	router := setupRouter()
-	w := httptest.NewRecorder()
-
-	rand.Seed(time.Now().UnixNano())
-	chars := []rune("abcdefghijklmnopqrstuvwxyz")
-	length := 8
-	var b strings.Builder
-	for i := 0; i < length; i++ {
-		b.WriteRune(chars[rand.Intn(len(chars))])
-	}
-	p := Product{
-		SKU:         b.String(),
-		Name:        "test1",
-		Price:       22,
-		Description: "used for testing",
-	}
-	pjson, _ := json.Marshal(p)
-
-	req, _ := http.NewRequest("POST", "/product", bytes.NewBuffer(pjson))
-	router.ServeHTTP(w, req)
-
-	assert.Equal(t, 201, w.Code)
 }
